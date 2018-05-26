244fc115d4ccdcb4507f38b5362d8a91ca5b68c5
brettelliot <brett@theelliots.net>
2018-05-26 14:50:44 -0400

    normal returns

--- examples/esfr_examples/esfr_4.py
+++ examples/esfr_examples/esfr_4.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 class EventStudyDataProvider(object):
     """Class that provides security data specifically for and EventStudy"""
@@ -14,7 +14,8 @@ class EventStudyDataProvider(object):
         columns = ['-6', '-5', '-4', '-3', '-2', '-1', '0', '1', '2', '3', '4', '5', '6']
         closing_prices_df = pd.DataFrame(index=[0], columns=columns)
 
-        prices = []
+        # Create a list of prices for the event window and fill with NaN
+        prices = [np.nan for i in range(num_pre_event_window_periods + 1 + num_post_event_window_periods)]
 
         if ticker == 'ALK':
             prices = [14.97, 15.16, 16.03, 15.84, 16.22, 15.84, 15.78, 15.81, 15.47, 15.25, 15.10, 14.94, 14.70]
@@ -24,6 +25,19 @@ class EventStudyDataProvider(object):
             prices = [12.53, 12.43, 12.36, 12.66, 12.48, 12.52, 12.31, 12.05, 12.01, 11.66, 11.54, 11.64, 11.70]
         elif ticker == 'UPS':
             prices = [86.79, 86.72, 86.96, 86.65, 86.61, 86.55, 86.30, 85.43, 85.52, 85.55, 85.77, 85.56, 86.43]
+        elif ticker == 'SPY':
+            if day_0_date == '2/1/2000':
+                prices = [1401.91, 1410.03, 1404.09, 1398.56, 1360.16, 1394.46, 1409.28, 1409.12, 1424.97, 1424.37,
+                          1423.00, 1441.75, 1411.71]
+            elif day_0_date == '11/12/2001':
+                prices = [1087.20, 1102.84, 1118.86, 1115.80, 1118.54, 1120.31, 1118.33, 1139.09, 1141.21, 1142.24,
+                          1138.65, 1151.06, 1142.66]
+            elif day_0_date == '4/2/2011':
+                prices = [1313.80, 1310.19, 1319.44, 1328.26, 1325.83, 1332.41, 1332.87, 1332.63, 1335.54, 1333.51,
+                          1328.17, 1324.46, 1314.16]
+            elif day_0_date == '8/14/2013':
+                prices = [1697.37, 1690.91, 1697.48, 1691.42, 1689.47, 1694.16, 1685.39, 1661.32, 1655.83, 1646.06,
+                          1652.35, 1642.80, 1656.96]
 
         closing_prices_df.loc[0] = prices
         return closing_prices_df
@@ -73,16 +87,48 @@ class EventStudy(object):
         self.num_post_event_window_periods = num_post_event_window_periods
 
         for index, event in self.event_list_df.iterrows():
-            print("\nEvent Day Zero: {} ticker: {}".format(event.day_0_date, event.ticker))
 
-            security_price_df = self.data_provider.get_closing_prices(event.ticker,
+            """
+            For each event:
+                Get the closing prices of the securities for the event window
+                Get the closing prices of the market benchamark for the event window
+                Calculate the returns for the security.
+                Calculate the and market around the event window
+            """
+
+            print('\nDay Zero Date: {} ticker: {}'.format(event.day_0_date, event.ticker))
+
+            # Get prices for the security over the event window
+            security_prices_df = self.data_provider.get_closing_prices(event.ticker,
                                                                       event.day_0_date,
                                                                       self.num_pre_event_window_periods,
                                                                       self.num_post_event_window_periods)
-            print("\n{} closing prices($):\n{}".format(event.ticker, security_price_df.to_string(index=False)))
 
-            security_returns_df = security_price_df.pct_change(axis='columns').round(4)
-            print("\n{} returns(%):\n{}".format(event.ticker, (security_returns_df*100).to_string(index=False)))
+            if security_prices_df.isnull().values.any():
+                print('\n**** Prices for {} are missing  around date: {} ****'.format(event.ticker, event.day_0_date))
+                continue
+
+            # Get prices for the market benchmark over the event window
+            market_prices_df = self.data_provider.get_closing_prices(market_ticker,
+                                                                    event.day_0_date,
+                                                                    self.num_pre_event_window_periods,
+                                                                    self.num_post_event_window_periods)
+
+            if market_prices_df.isnull().values.any():
+                print('\n**** Prices for {} are missing around date: {} ****'.format(market_ticker, event.day_0_date))
+                continue
+
+            # print('\n{} Security prices($):\n{}'.format(event.ticker, security_prices_df.to_string(index=False)))
+            #print('\n{} Market prices($):\n{}'.format(market_ticker, market_prices_df.to_string(index=False)))
+
+            # Calculate the arithmetic return for the security over the event window
+            security_returns_df = security_prices_df.pct_change(axis='columns').round(4)
+            #print('\nSecurity Returns(%) for {}:\n{}'.format(event.ticker, (security_returns_df*100).to_string(index=False)))
+
+            # Calculate the arithmetic return for the market over the event window.
+            # In the naive model, this becomes the Normal Return.
+            normal_returns_df = market_prices_df.pct_change(axis='columns').round(4)
+            print('\nNormal Returns(%) for {}:\n{}'.format(market_ticker, (normal_returns_df*100).to_string(index=False)))
 
         return self.results
 
@@ -93,8 +139,8 @@ def load_events(file_name):
     event_list_df = pd.read_csv(file_name,
                                 encoding='ISO-8859-1',
                                 usecols=['day_0_date', 'ticker'],
-                                #parse_dates=['day_0_date'],
-                                #index_col='day_0_date',
+                                # parse_dates=['day_0_date'],
+                                # index_col='day_0_date',
                                 )
 
     return event_list_df
@@ -108,7 +154,7 @@ def main():
     """
 
     event_list_df = load_events('esfr_4_events.csv')
-    #print('The event list:\n {}'.format(event_list_df))
+    # print('The event list:\n {}'.format(event_list_df))
 
     data_provider = EventStudyDataProvider()
 
