cffc6a80dbc4b33e68e863488428996af51cc991
brettelliot <brett@theelliots.net>
2018-08-26 11:20:22 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='eventstudy',
-      version='0.0.2',
+      version='0.0.3',
       description='An event study tool',
       url='http://github.com/brettelliot/event-study',
       author='Brett Elliot',
       author_email='brett@theelliots.net',
       license='MIT',
-      packages=['eventstudy'],
+      packages=find_packages(),
       zip_safe=False)
