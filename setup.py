195ff672ce2c2248f47a42c309ee539b955951b7
brettelliot <brett@theelliots.net>
2018-08-26 11:01:46 -0400

    init.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='eventstudy',
-      version='0.0.1',
+      version='0.0.2',
       description='An event study tool',
       url='http://github.com/brettelliot/event-study',
       author='Brett Elliot',
